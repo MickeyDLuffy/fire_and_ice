7dcd7a76b5ebeb523052164f962e3605850dd315
MickeyDLuffy <thefalconslayer@gmail.com>
2023-05-05 19:46:39 +0000

    update to angular 16

--- apps/fire_and_ice/src/app/core/services/book.service.ts
+++ apps/fire_and_ice/src/app/core/services/book.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { BookFilter, BookModel } from '../models/book.model';
 import { HttpClient } from '@angular/common/http';
 import { GenericService } from './generic.service';
-import { of, zipWith } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,11 +10,6 @@ export class BookService extends GenericService<BookModel, BookFilter> {
   protected url = '/books';
 
   constructor(override httpClient: HttpClient) {
-    const firstObservable = of('hello');
-    const secondObservable = of('world');
-    const thirdObservable = of('!');
-    const fourthObservable = of('RxJS');
-
     super(httpClient);
   }
 }
